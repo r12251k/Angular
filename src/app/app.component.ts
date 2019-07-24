d4f7523f079f868e220ab7c898b1037293eac045
ROOSEWELT_KOMMU <powerofroos@gmail.com>
2019-07-24 18:49:42 +0530

    data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Data';
+  firstname:string="ROOSEWELT";
+  lastname:string="KOMMU";
+  age:number=21;
+  class:string="BTECH";
+  smokes:boolean=true;
+  gender:string="male";
+  attitude:string="not abusive";
+  msg:string=""
+  changeData(){
+    this.msg=this.firstname="nani";
+    this.lastname="kommu";
+    this.age=20;
+    this.class="btech3";
+    this.smokes=false;
+    this.gender="MALE";
+    this.attitude="ABUSIVE"; 
+  }
 }
